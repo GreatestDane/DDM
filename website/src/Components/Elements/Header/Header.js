5086e2f24ba2976c8a0f92f8bdad7cc5284b4ef0
Dane M Goodman <DaneGoodman85@gmail.com>
2020-06-14 17:01:20 -0700

    Built the header component

---
+++ website/src/Components/Elements/Header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'; 
+import './Header.css';
+
+const Header = () => {
+    return(
+        <div className="header">
+            <div className="header-content">
+                <h1>Dane and the Death Machine</h1>
+            </div>
+        </div>
+    )
+}
+
+export default Header;
