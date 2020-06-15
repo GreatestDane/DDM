61aa6965a7cbe54899395ae58426f01e7074abed
Dane M Goodman <DaneGoodman85@gmail.com>
2020-06-14 17:03:05 -0700

    Changed the App component

--- website/src/Components/App/App.js
+++ website/src/Components/App/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import Header from '../Elements/Header/Header.js'
 import Home from '../Home/Home.js'
+
 
 function App() {
   return (
     <div>
+      <Header />
       <Home />
     </div>
   );
