c232a352e183be8c57eef8db44a59c3b54516ab6
Dane M Goodman <DaneGoodman85@gmail.com>
2020-06-19 15:28:36 -0700

    Added the navigation links to the header, still gotta change the font

--- website/src/Components/Elements/Header/Header.js
+++ website/src/Components/Elements/Header/Header.js
@@ -2,10 +2,20 @@ import React from 'react';
 import './Header.css';
 
 const Header = () => {
+
+    const navigationItems = ["Videos", "Lyrics", "Photo Gallery", "Blog"]
+
+    const listNavItems = navigationItems.map((items) => {
+        return <li>{items}</li>
+    })
+
     return(
         <div className="header">
             <div className="header-content">
                 <h1>Dane and the Death Machine</h1>
+                <ul>     
+                    {listNavItems}
+                </ul>
             </div>
         </div>
     )
