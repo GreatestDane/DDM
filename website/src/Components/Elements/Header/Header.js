f77b48bb51d8d1f22b71979ea6478cbba8a17422
Dane M Goodman <DaneGoodman85@gmail.com>
2020-07-07 21:45:40 -0700

    Took out the map on the header links for router

--- website/src/Components/Elements/Header/Header.js
+++ website/src/Components/Elements/Header/Header.js
@@ -3,18 +3,21 @@ import './Header.css';
 
 const Header = () => {
 
-    const navigationItems = ["Videos", "Lyrics", "Photo Gallery", "Blog"]
+    // const navigationItems = ["Videos", "Lyrics", "Photo Gallery", "Blog"]
 
-    const listNavItems = navigationItems.map((items) => {
-        return <li>{items}</li>
-    })
+    // const listNavItems = navigationItems.map((items) => {
+    //     return <li>{items}</li>
+    // })
 
     return(
         <div className="header">
             <div className="header-content">
                 <h1>Dane and the Death Machine</h1>
                 <ul>     
-                    {listNavItems}
+                    <li>VIDEOS</li>
+                    <li>LYRICS</li>
+                    <li>PHOTO GALLERY</li>
+                    <li>BLOG</li>
                 </ul>
             </div>
         </div>
