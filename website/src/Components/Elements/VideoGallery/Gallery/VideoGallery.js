24e9c99eead27d6ae13316d09f33788d8893943c
Dane M Goodman <DaneGoodman85@gmail.com>
2020-07-13 22:00:27 -0700

    Nevermind, I just didn't have my server running

--- website/src/Components/Elements/VideoGallery/Gallery/VideoGallery.js
+++ website/src/Components/Elements/VideoGallery/Gallery/VideoGallery.js
@@ -4,13 +4,15 @@ import './VideoGallery.css';
 class VideoGallery extends Component {
 
     state = {
-        joes: <img src='/images/joe.jpg' classname='images' alt='Live at Joes'></img>
+        joes: <img src='/images/joe.jpg' className='images' alt='Live at Joes'></img>,
+        crab: <img src='/images/background.jpg' className='images' alt='test'></img>
     }
 
     render() {
         return (
             <div id="flex-container">
                 <div id='fuck'>WHAT THE HOLY FUCK</div>
+               {this.state.joes}
                {this.state.joes}
                {/* <img classname='images' src='/images/joe.jpg'></img> */}
             </div>
